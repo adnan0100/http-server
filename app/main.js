b569da40ae1580b1e5d1403f3ad64bd400c925a8
Adnan <adnanrocker100@gmail.com>
2024-12-01 13:47:37 +0500

    Commit done

--- app/main.js
+++ app/main.js
@@ -16,5 +16,5 @@ server.listen(8080, () => {
   console.log("Server is running on port 8080");
   console.log("Server is running on port 8080");
   console.log("Server is running on port 8080");
-  console.log("Server is running on port 8080");
+  console.log("Server is running on port 4221");
 });
