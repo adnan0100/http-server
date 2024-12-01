eb0ed19c9fb4ef24a350a5f5be0e72d7d9d57952
Adnan <adnanrocker100@gmail.com>
2024-12-01 13:17:00 +0500

    first commit

---
+++ app/main.js
@@ -0,0 +1,18 @@
+const net = require("net");
+
+const server = net.createServer((socket) => {
+  socket.write("HTTP/1.1 200 OK\r\n\r\n");
+  
+  // Use 'end' to close the connection gracefully
+  socket.end();
+  
+  socket.on("close", () => {
+    console.log("Socket closed");
+  });
+});
+
+// Start the server
+server.listen(8080, () => {
+  console.log("Server is running on port 8080");
+  console.log("Server is running on port 8080");
+});
